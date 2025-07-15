be5d90811f55a50179bf76faffa47fa273666dbc
gavinswilson <gavinswilson@gmail.com>
2025-07-15 06:26:09 -0700

    post training

---
+++ Itertools/read.md
@@ -0,0 +1,7 @@
+# references
+
+## websites
+https://docs.python.org/3/library/itertools.html
+
+
+## functions
---
+++ test/test.py
@@ -0,0 +1,12 @@
+
+
+if __name__ == "__main__":
+    import sys
+    from pathlib import Path
+
+    # Ensure the script is run from the correct directory
+    # current_path = Path(__file__).resolve()
+    # if current_path.parent != Path.cwd():
+    #     print(f"Please run this script from the directory: {current_path.parent}")
+    #     sys.exit(1)
+    print("Script is running in the correct directory.")
